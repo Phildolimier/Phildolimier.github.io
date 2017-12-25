6c6796405a76c131db2bf068baae7a87c5a3a813
Philippe Dolimier <10210537+Phildolimier@users.noreply.github.com>
2017-12-25 00:36:48 -0500

    V1 Design Layout

--- functions.js
+++ functions.js
@@ -1,8 +1,19 @@
-// With the element initially shown, we can hide it slowly:
+// Hide 1 DOM
 function hide(id) {
   var idObject = document.getElementById (id) ;
 
     id.style.visibility = "hidden";
+}
+
+// Hide multiple DOM at once
+function hide2(id1, id2) {
+  var idObject1 = document.getElementById (id1) ;
+  var idObject2 = document.getElementById (id2) ;
+
+
+    id1.style.visibility = "hidden";
+    id2.style.visibility = "hidden";
+
 }
 
 function show(id) {
